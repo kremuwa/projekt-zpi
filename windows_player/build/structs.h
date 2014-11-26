c3f69266101ed81825b76f38709be287dbc7f941
Kozub <kozubg@gmail.com>
2014-11-26 03:02:28 +0100

    wydzielenie struktur

---
+++ windows_player/build/structs.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <utility>
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h> 
+
+typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
+
+typedef struct {
+	std::string name;
+	double x_min;
+	double x_max;
+	double y_min;
+	double y_max;
+	double z_min;
+	double z_max;
+} cubeStruct;
+
+typedef struct {
+	PointCloudT* cloud;
+	int frame_time;
+	std::vector<cubeStruct> people;
+} frameStruct;
